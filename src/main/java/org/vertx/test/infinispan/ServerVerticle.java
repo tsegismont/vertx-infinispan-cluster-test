cdb9bf150d094a4334acc7a64f8c948bf5579ac1
Thomas Segismont <tsegismo@redhat.com>
2021-11-23 16:41:10 +0100

    Simple reproducer

--- src/main/java/org/vertx/test/infinispan/ServerVerticle.java
+++ src/main/java/org/vertx/test/infinispan/ServerVerticle.java
@@ -1,63 +1,32 @@
 package org.vertx.test.infinispan;
 
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Vertx;
-import io.vertx.core.VertxOptions;
-import io.vertx.core.spi.cluster.ClusterManager;
-import io.vertx.ext.cluster.infinispan.InfinispanClusterManager;
-import io.vertx.ext.web.Router;
-import io.vertx.ext.web.Session;
-import io.vertx.ext.web.handler.SessionHandler;
-import io.vertx.ext.web.sstore.ClusteredSessionStore;
-import io.vertx.ext.web.sstore.SessionStore;
+import io.vertx.core.*;
+import io.vertx.core.eventbus.Message;
 
 import java.util.Date;
 
-public class ServerVerticle extends AbstractVerticle {
-
-  public static final String TEMPLATE = ""
-    + "Session [%s] created on %s%n"
-    + "%n"
-    + "Page generated on %s%n";
+public class ServerVerticle extends AbstractVerticle implements Handler<Message<String>> {
 
   public static void main(String[] args) {
-    ClusterManager mgr = new InfinispanClusterManager();
-
-    VertxOptions options = new VertxOptions().setClusterManager(mgr);
-
-    Vertx.clusteredVertx(options, res -> {
-      if (res.succeeded()) {
-        Vertx vertx = res.result();
-        vertx.deployVerticle(new ServerVerticle());
+    Vertx.clusteredVertx(new VertxOptions()).compose(vertx -> vertx.deployVerticle(new ServerVerticle())).onComplete(ar -> {
+      if (ar.succeeded()) {
+        System.out.println(">>> Verticle deployed");
       } else {
-        // failed!
+        ar.cause().printStackTrace();
       }
     });
-
-
   }
 
   @Override
-  public void start() {
-    Router router = Router.router(vertx);
-
-//    SessionStore store = ClusteredSessionStore.create(vertx);
-//
-//    router.route().handler(SessionHandler.create(store));
-
-    router.get("/").handler(ctx -> {
-      Session session = ctx.session();
-      session.computeIfAbsent("createdOn", s -> System.currentTimeMillis());
-
-      String sessionId = session.id();
-      Date createdOn = new Date(session.<Long>get("createdOn"));
-      Date now = new Date();
-
-      ctx.end(String.format(TEMPLATE, sessionId, createdOn, now));
+  public void start(Promise<Void> startPromise) throws Exception {
+    vertx.eventBus().consumer("foo", this).completionHandler(ar -> {
+      startPromise.handle(ar);
+      vertx.eventBus().publish("foo", "Server started " + new Date());
     });
+  }
 
-    vertx.createHttpServer()
-      .requestHandler(router)
-      .listen(8080);
+  @Override
+  public void handle(Message<String> msg) {
+    System.out.println("msg = " + msg.body());
   }
 }
